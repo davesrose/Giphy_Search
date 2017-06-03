ed14ae8bfa0bb40769b4f4d527f9b573e69dc9b0
davesrose <dsr0116@yahoo.com>
2017-06-02 21:38:42 -0400

    made responsive and resolved some broken code

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,7 @@
 $(document).ready(function() {
 
+	//setting topics string
+	var j;
 	var topics = [
 		"Star Trek",
 		"leonard mccoy Im a doctor",
@@ -11,15 +13,19 @@ $(document).ready(function() {
 		"kirk wrath khan",
 		"Spock",
 		"Star Trek Q",
-		"Jim Kirk"
+		"James T Kirk"
 
-	];
+	], j;
 
+	//function for generating buttons from the topics array
 	function displayButtons() {
-		$(".buttons").empty();
-		var btnName = []
+
+		$(".buttons").empty();  //empty any previously generated buttons
+
+		var btnName = []  //creating btnName array to rename complex search terms to simpler button names
 		for(j = 0; j < topics.length; j++) {
-			btnName[j] = topics[j]
+			btnName[j] = topics[j] //setting btnNames to same value as topics values
+			//creating if statements to change btnNames
 			if (topics[1] === "leonard mccoy Im a doctor") {
 				btnName[1] = "Bones"
 			}
@@ -41,13 +47,15 @@ $(document).ready(function() {
 			if (topics[9] === "Star Trek Q") {
 				btnName[9] = "Q"
 			}
-			var showButtons = $('<div class="btn"><button class="btn-gif" data-subject="' + topics[j] + '"><span>' + btnName[j] + '</span></button></div>');
-			$('.buttons').append(showButtons);
+			var showButtons = $('<div class="btn"><button class="btn-gif" data-subject="' + topics[j] + '"><span>' + btnName[j] + '</span></button></div>');  //creating container divs for buttons that have topics search variable and visible labels as btnName
+			$('.buttons').append(showButtons);  //append each button
 		}
-		$('.buttons').append('<div style="clear: both;"></div>');
+		$('.buttons').append('<div style="clear: both;"></div>'); //clear CSS float left property
 
 	}
+	displayButtons(); //initially run to display topics array
 
+	//an initial function for displaying Star Trek gifs when the page loads
 	function initialThumbs() {
 		var queryUrl = ('https://api.giphy.com/v1/gifs/search?q=star trek&limit=10&rating=pg-13&api_key=dc6zaTOxFJmzC');
 		console.log(queryUrl);
@@ -56,81 +64,88 @@ $(document).ready(function() {
 			console.log(response);
 
 			for(var i = 0; i < response.data.length; i++) {
-				var rating = response.data[i].rating.toUpperCase();
-				var stillImage = response.data[i].images.original_still.url;
-				var animatedImage = response.data[i].images.original.url;
+				var rating = response.data[i].rating.toUpperCase();  //giphys ratings are lowercase by default, so I changed them to uppercase
+				var stillImage = response.data[i].images.original_still.url;  //getting the still gif link
+				var animatedImage = response.data[i].images.original.url;  //getting the original animated gif link
 
-				$('.gifs').append($('<div class="gif"><img class="gif-thumb" alt="" src="' + 
+				$('.gifs').append($('<div class="gif"><img class="gif-thumb" alt="" src="' +  //src is initially still url
 					stillImage 
-					+ '" data-state="still" data-animate="' + 
+					+ '" data-state="still" data-animate="' + //setting initial data-state to still and store an animated and still data value
 					animatedImage
 					+ '" data-still="' + 
 					stillImage
-					+ '" /><p>' + rating + '</p></div>'));
+					+ '" /><p>rating: ' + rating + '</p></div>'));  //putting rating at bottom of image
 			}
-			$('.gifs').append('<div style="clear: both;"></div>');
-			$('.gif-thumb').on('click', gifClick); // end img on click
+			$('.gifs').append('<div style="clear: both;"></div>');  //clear CSS float left property
+			$('.gif-thumb').on('click', gifClick);  // run gifClick when gif image is clicked on
 		})
 
 	}
-	initialThumbs();
+	initialThumbs(); //initially run initialThumbs so that page isn't blank when first loaded
 
+	//function for displaying giffs when button is pressed
 	function displayThumbs(event) {
-		$(".gifs").empty();
-		var btnTopic = $(this).attr("data-subject");
-		console.log(btnTopic)
-		var queryUrl = ('https://api.giphy.com/v1/gifs/search?q=' + btnTopic + '&limit=10&rating=pg-13&api_key=dc6zaTOxFJmzC');
+		$(".gifs").empty();  //clear previous gifs
+		var btnTopic = $(this).attr("data-subject");  //setting btnTopic variable as button's data-subject attribute
+		//creating if statement to pass search value as btnTopic if function being run through search
+		if ($("#input-gif").val() === "") {
+			btnTopic = $(this).attr("data-subject");  //value stays the same if search field is empty
+		} else {
+			btnTopic = $("#input-gif").val().trim();  //value is the search value if posted
+		}
+		var queryUrl = ('https://api.giphy.com/v1/gifs/search?q=' + btnTopic + '&limit=10&rating=pg-13&api_key=dc6zaTOxFJmzC');  //adding topic to search string
 		console.log(queryUrl);
 		$.ajax({url: queryUrl, method: 'GET'})
 		.done(function(response) {
 			console.log(response);
 
 			for(var i = 0; i < response.data.length; i++) {
-				var rating = response.data[i].rating.toUpperCase();
-				var stillImage = response.data[i].images.original_still.url;
-				var animatedImage = response.data[i].images.original.url
+				var rating = response.data[i].rating.toUpperCase();  //giphys ratings are lowercase by default, so I changed them to uppercase
+				var stillImage = response.data[i].images.original_still.url;  //getting the still gif link
+				var animatedImage = response.data[i].images.original.url  //getting the original animated gif link
 
-				$('.gifs').append($('<div class="gif"><img class="gif-thumb" alt="" src="' + 
+				$('.gifs').append($('<div class="gif"><img class="gif-thumb" alt="" src="' +  //src is initially still url 
 					stillImage 
-					+ '" data-state="still" data-animate="' + 
+					+ '" data-state="still" data-animate="' + //setting initial data-state to still and store an animated and still data value 
 					animatedImage
 					+ '" data-still="' + 
 					stillImage
-					+ '" /><p>' + rating + '</p></div>'));
+					+ '" /><p>rating: ' + rating + '</p></div>'));  //putting rating at bottom of image
 			}
-			$('.gifs').append('<div style="clear: both;"></div>');
-			$('.gif-thumb').on('click', gifClick); // end img on click
+			$('.gifs').append('<div style="clear: both;"></div>');  //clear CSS float left property
+			$('.gif-thumb').on('click', gifClick);  // run gifClick when gif image is clicked on
 		})
 
 	}
 
+	//search function
     $("#add-gif").on("click", function(event) {
     	event.preventDefault();
-        var topic = $("#input-gif").val().trim();
+    	var topic = $("#input-gif").val().trim();  //setting search value as topic variable
         if (topic !== "") {
-	        console.log(topic);
-	        topics.push(topic);
-	        // console.log(topics)
-	        displayButtons();
+        	topics.push(topic);  //push topic variable into topics array
+        	jQuery.unique(topics);  //jQuery method for removing duplicates from the topics array
+	        displayButtons();  //run displayButtons function to refresh buttons
+	        displayThumbs();  //run displayThumbs function to refresh GIF display
+			$("#input-gif").val('');  //return search field to empty
     	} else {
     		return topic;
     	}
+		
     });
 
-	displayButtons();
-
-	function gifClick(event) {
-		var state = $(event.target).attr('data-state');
-		if (state === "still") {
-			$(event.target).attr('src', $(event.target).data('animate'));
-			$(event.target).attr('data-state', 'animate');
+	function gifClick(event) {  //function for animating gif when clicked
+		var state = $(this).attr("data-state");  //creating variable for gif's data-state
+		if (state === "still") {  //if state is static, will animate
+			$(this).attr("src", $(this).data("animate")); //pass image's src with data-animate url
+			$(this).attr("data-state", "animate"); //set data-state to animate
 		} else {
-			$(event.target).attr('src', $(event.target).data('still'));
-			$(event.target).attr('data-state', 'still');
+			$(this).attr("src", $(this).data("still"));  //pass image's src with data-still url
+			$(this).attr("data-state", "still");  //set data-state to still
 		}
 	}
 
-	$(document).on("click", ".btn-gif", displayThumbs);
+	$(document).on("click", ".btn-gif", displayThumbs); //when page is loaded, goto displayThumbs function when button is pressed
 
 
 });
