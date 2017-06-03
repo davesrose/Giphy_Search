a222fd52f4573e0d0d0b0e20f95acec475515a00
davesrose <dsr0116@yahoo.com>
2017-06-03 16:44:09 -0400

    trying another hack

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -83,6 +83,7 @@ $(document).ready(function() {
 			}
 			$('.gifs').append('<div style="clear: both;"></div>');  //clear CSS float left property
 			$('.gif-thumb').on('touchstart click', gifClick);  // run gifClick when gif image is clicked on
+			$('.gif-thumb').live('click touchstart', gifClick);  // run gifClick when gif image is clicked on
 		})
 
 	}
@@ -118,7 +119,8 @@ $(document).ready(function() {
 					+ '" /><p>rating: ' + rating + '</p></div>'));  //putting rating at bottom of image
 			}
 			$('.gifs').append('<div style="clear: both;"></div>');  //clear CSS float left property
-			$('.gif-thumb').on('touchstart click', gifClick);  // run gifClick when gif image is clicked on
+			$('.gif-thumb').on('click touchstart', gifClick);  // run gifClick when gif image is clicked on
+			$('.gif-thumb').live('click touchstart', gifClick);  // run gifClick when gif image is clicked on
 		})
 
 	}
